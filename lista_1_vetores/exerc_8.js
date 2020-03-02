d891eb0a94c8a5f3e55cb5586cfe49e8d3a04341
Ricardo Carvalho <ricardo2010carvalho@hotmail.com>
2020-03-02 13:00:57 -0300

    Subindo o exerc 8

--- lista_1_vetores/exerc_8.js
+++ lista_1_vetores/exerc_8.js
@@ -7,3 +7,40 @@ const assert = require('assert');
 * posição escolhida: 2
 * vetor_cópia [ 5.6, 7.1, 2.8]
  */
+
+var vetor = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+var vetor2 = [];
+var num = 8;
+var teste = false;
+
+if (num < 7){
+    for (let i = num; i < num + 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            vetor2[j] = vetor[i];
+            i++;
+        }
+    }
+}
+else {
+    for (let i = num; i < vetor.length; i++) {
+        if(teste == false) {
+            for (let j = 0; j < 3; j++) {
+                while (vetor[i] != undefined && i > 6) {
+                    vetor2[j] = vetor[i];
+                    j++;
+                    i++;
+                }
+                    vetor2[j] = vetor[i - vetor.length];
+                    vetor.length--;
+
+                if(vetor2.length == 3){
+                    teste = true;
+                }
+
+
+            }
+        }
+    }
+}
+
+console.log(vetor2);
