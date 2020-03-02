a52ffb77216fa084dc95e6af80228c99501971ee
ricardo carvalho <ricardo2010carvalho@hotmail.com>
2020-03-02 11:05:09 -0300

    add exerc-10

--- lista_1_vetores/exerc_9.js
+++ lista_1_vetores/exerc_9.js
@@ -3,5 +3,30 @@ var assert = require('assert');
 /*
 * Faça um programa que receba e grave um vetor de 10 elementos inteiros e mostre qual é o maior e qual é
 * menor valor desse vetor.
-*
  */
+
+var vetor = [ 8, 7, 6, 5, 4, 3, 2, 1, 0];
+var aux = 0;
+var controle;
+
+    for (let i = 0; i < vetor.length; i++) {
+        controle = true;
+        for (let j = 0; j < vetor.length; j++) {
+            if (vetor[j] > vetor[j + 1]){
+                aux = vetor[j];
+                vetor[j] = vetor[j + 1];
+                vetor[j + 1] = aux;
+                controle = false;
+            }
+        }
+        if(controle){
+            break;
+        }
+    }
+
+try{
+    assert.deepEqual([0, 1, 2, 3, 4, 5, 6, 7, 8], vetor);
+}catch (e) {
+    console.log(e);
+}
+console.table(vetor);
