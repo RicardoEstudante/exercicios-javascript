3587977d8d9aa692300aed2de8e942d97449687e
ricardo carvalho <ricardo2010carvalho@hotmail.com>
2020-03-02 02:03:01 -0300

    add mais exercs

---
+++ lista_1_vetores/exerc_8.js
@@ -0,0 +1,9 @@
+const assert = require('assert');
+
+/*
+* Faça um programa que peça ao usuário preencher 10 valores reais em um vetor e depois peça para que ele
+* escolha um índice (de 0 - 9). A partir desse índice, copie 3 valores para um segundo vetor. Exemplo:
+* vetor_original [2.3, 4.2, 5.6, 7.1, 2.8, 9.0, 1.0, 4.8, 5.5, 6.3]
+* posição escolhida: 2
+* vetor_cópia [ 5.6, 7.1, 2.8]
+ */
---
+++ lista_1_vetores/exerc_9.js
@@ -0,0 +1,7 @@
+var assert = require('assert');
+
+/*
+* Faça um programa que receba e grave um vetor de 10 elementos inteiros e mostre qual é o maior e qual é
+* menor valor desse vetor.
+*
+ */
